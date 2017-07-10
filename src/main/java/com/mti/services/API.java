f5fb76a20cfce73e8210853a9bf703d5c007534f
Zibe <jbaptiste.herve@gmail.com>
2017-07-10 16:51:06 +0200

    Init API application, CrossDomain enabled

---
+++ src/main/java/com/mti/services/API.java
@@ -0,0 +1,22 @@
+package com.mti.services;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Set;
+
+/**
+ * Created by Zibe on 10/07/2017.
+ */
+@ApplicationPath("/api")
+public class API extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        return getRestResourceClasses();
+    }
+    private Set<Class<?>> getRestResourceClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<Class<?>>();
+        //WS Files
+        // Example : resources.add(UserWebService.class);
+        return resources;
+    }
+}
